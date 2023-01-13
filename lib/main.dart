4e9c6bc09ba540019d703a2e1916e171c38635f3
JastonRuggy <jastonruggy@icloud.com>
2023-01-13 19:51:17 +0300

    ef

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, annotate_overrides, sort_child_properties_last
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, annotate_overrides, sort_child_properties_last, avoid_unnecessary_containers, sized_box_for_whitespace
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -24,7 +24,15 @@ class MyHomePage extends StatelessWidget {
         // ignore: prefer_const_literals_to_create_immutables
         children: <Widget>[
           Card(
-            child: Text('CHART'),
+            color: Colors.orange,
+            child: Container(
+              // color: Color.fromARGB(255, 248, 46, 164),
+
+              // The double.infinity property means take as much width as you can on a certain device
+              width: double.infinity,
+              // width: 100,
+              child: Text('CHART'),
+            ),
             elevation: 5,
           ),
           Card(
