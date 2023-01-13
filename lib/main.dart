0c8dbd6c9fdfa3905c46c6662f94124c97fa7c2d
JastonRuggy <jastonruggy@icloud.com>
2023-01-13 16:06:13 +0300

    ef

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, annotate_overrides
 import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(
+        child: Text('Widget Playground'),
+      ),
+    );
+  }
+}
