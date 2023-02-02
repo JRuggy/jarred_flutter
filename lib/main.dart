fb6ce06c8eefe1354ff9db86543c25be5a2ead7d
JastonRuggy <jastonruggy@icloud.com>
2023-02-02 15:20:00 +0300

    rg

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, annotate_overrides, sort_child_properties_last, avoid_unnecessary_containers, sized_box_for_whitespace
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, annotate_overrides, sort_child_properties_last, avoid_unnecessary_containers, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, duplicate_ignore
 import 'package:flutter/material.dart';
+import 'package:jarred/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Shorts',
+      amount: 70.10,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,13 +54,34 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Container(
-              // width: 90,
-              width: double.infinity,
-              color: Color.fromARGB(255, 43, 255, 0),
-              child: Text('List Of Transactions'),
-            ),
+          // Card(
+          //   child: Container(
+          //     // width: 90,
+          //     width: double.infinity,
+          //     color: Color.fromARGB(255, 43, 255, 0),
+          //     child: Text('List Of Transactions'),
+          //   ),
+          // ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        tx.amount.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
